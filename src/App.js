70959a947aa7e3566bcbdca5505105f88301b973
Jose Angel <garizabalo@google.com>
2020-07-23 20:40:56 -0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="course-goals">
+      <h2>Course goals</h2>
+    </div>
+  );
+}
+
+export default App;
